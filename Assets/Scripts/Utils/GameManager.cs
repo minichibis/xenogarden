5e8fd853b3db2dfd1a4672e6963a90e39a50cf0b
Wolfgang Gross <60198468+WolfgangPGross@users.noreply.github.com>
2021-02-28 18:06:44 -0600

    TitleScreen Implementation
    Need to fix issue with tab Images not correctly being display on first MouseOver

---
+++ Assets/Scripts/Utils/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    //public GameObject pauseMenu;
+
+    private string CurrentLevelName = string.Empty;
+
+    #region This code makes this class a Singleton
+    //Done by Singleton Class
+    /*
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            //Make sure this game manager persists across scenes\
+            DontDestroyOnLoad(gameObject);
+            
+            
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("Trying to instatiate a second instance of singleton Game Manager");
+        }
+
+
+    }
+    */
+    #endregion
+
+    //methods to load and unload scenes
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to load level " + levelName);
+            return;
+        }
+        CurrentLevelName = levelName;
+    }
+
+    public void UnLoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to unload level " + levelName);
+            return;
+        }
+    }
+
+    public void UnloadCurrentLevel()
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(CurrentLevelName);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to unload level " + CurrentLevelName);
+            return;
+        }
+    }
+
+    //pausing and unpausing
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        //pauseMenu.SetActive(true);
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1f;
+        //pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+        }
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            UnPause();
+        }
+    }
+
+
+
+}
