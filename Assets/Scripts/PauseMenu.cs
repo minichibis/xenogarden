87ada8412c0a7acc66537f7993d809912b2621b3
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-03-22 00:10:08 -0500

    Added encyclopedia to pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject encyclopediaScreen;
+    private bool inEncyclopedia = false;
+
     public void pause()
     {
         Time.timeScale = 0;
@@ -20,5 +23,23 @@ public class PauseMenu : MonoBehaviour
     public void restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void encyclopedia()
+    {
+        inEncyclopedia = true;
+        encyclopediaScreen.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (inEncyclopedia)
+            {
+                encyclopediaScreen.SetActive(false);
+                inEncyclopedia = false;
+            }
+        }
     }
 }
